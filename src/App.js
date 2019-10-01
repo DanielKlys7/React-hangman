c23af6df7835dc4b883ba51ecd76629050093461
Daniel Kłys <danielklys7@gmail.com>
2019-10-01 20:18:36 +0200

    get rid of this long switch replacing it by regex

--- src/App.js
+++ src/App.js
@@ -56,88 +56,9 @@ class App extends Component {
   }
 
   keypressHandler = (e) => {
-    switch (e.keyCode) {
-      case 81:
-        this.keypressCheckFunction('q')
-        break;
-      case 87:
-        this.keypressCheckFunction('w')
-        break;
-      case 69:
-        this.keypressCheckFunction('e')
-        break;
-      case 82:
-        this.keypressCheckFunction('r')
-        break;
-      case 84:
-        this.keypressCheckFunction('t')
-        break;
-      case 89:
-        this.keypressCheckFunction('y')
-        break;
-      case 85:
-        this.keypressCheckFunction('u')
-        break;
-      case 73:
-        this.keypressCheckFunction('i')
-        break;
-      case 79:
-        this.keypressCheckFunction('o')
-        break;
-      case 80:
-        this.keypressCheckFunction('p')
-        break;
-      case 65:
-        this.keypressCheckFunction('a')
-        break;
-      case 83:
-        this.keypressCheckFunction('s')
-        break;
-      case 68:
-        this.keypressCheckFunction('d')
-        break;
-      case 70:
-        this.keypressCheckFunction('f')
-        break;
-      case 71:
-        this.keypressCheckFunction('g')
-        break;
-      case 72:
-        this.keypressCheckFunction('h')
-        break;
-      case 74:
-        this.keypressCheckFunction('j')
-        break;
-      case 75:
-        this.keypressCheckFunction('k')
-        break;
-      case 76:
-        this.keypressCheckFunction('l')
-        break;
-      case 90:
-        this.keypressCheckFunction('z')
-        break;
-      case 88:
-        this.keypressCheckFunction('x')
-        break;
-      case 67:
-        this.keypressCheckFunction('c')
-        break;
-      case 86:
-        this.keypressCheckFunction('v')
-        break;
-      case 66:
-        this.keypressCheckFunction('b')
-        break;
-      case 78:
-        this.keypressCheckFunction('n')
-        break;
-      case 77:
-        this.keypressCheckFunction('m')
-        break;
-      default:
-        console.log('choose your letter');
-        break;
+    let regex = /^[a-z]$/i;
+    if (regex.test(e.key)) {
+      this.keypressCheckFunction(e.key)
     }
   }
 
